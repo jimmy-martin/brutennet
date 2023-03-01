1b50bf5bd29b0a48503017ac1b6fcfb64a8f0409
jimmy-martin <jimmy.martin952@gmail.com>
2023-03-01 22:50:22 +0100

    all conversions done

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,9 @@
 const app = {
   taxes: 0.22,
+  annualGrossSalary: 0,
+  annualNetSalary: 0,
+  monthlyGrossSalary: 0,
+  monthlyNetSalary: 0,
 
   init: () => {
     app.bindEvents();
@@ -15,56 +19,84 @@ const app = {
     return Math.round(annualGrossSalary);
   },
 
+  annualSalaryToMonthly: (annualSalary) => {
+    const monthlySalary = annualSalary / 12;
+    return Math.round(monthlySalary);
+  },
+
+  monthlySalaryToAnnual: (monthlySalary) => {
+    const annualSalary = monthlySalary * 12;
+    return Math.round(annualSalary);
+  },
+
   bindEvents: () => {
-    const annualGrossInput = document.querySelector('#gross_input');
-    annualGrossInput.addEventListener('input', app.updateAnnualNetSalary);
+    const annualGrossInput = document.querySelector('#annual_gross_input');
+    annualGrossInput.addEventListener('input', app.updateSalary);
+    const monthlyGrossInput = document.querySelector('#monthly_gross_input');
+    monthlyGrossInput.addEventListener('input', app.updateSalary);
 
-    const annualNetInput = document.querySelector('#net_input');
-    annualNetInput.addEventListener('input', app.updateAnnualGrossSalary);
+    const annualNetInput = document.querySelector('#annual_net_input');
+    annualNetInput.addEventListener('input', app.updateSalary);
+    const monthlyNetInput = document.querySelector('#monthly_net_input');
+    monthlyNetInput.addEventListener('input', app.updateSalary);
   },
 
-  updateAnnualNetSalary: (evt) => {
-    const annualGrossInput = evt.target;
+  updateSalary: (evt) => {
+    const input = evt.target;
+    const id = input.id;
 
-    const annualNetInput = document.querySelector('#net_input');
-    const annualNetSalarySpan = document.querySelector('#net_salary');
-    const annualGrossSalarySpan = document.querySelector('#gross_salary');
-
-    if (annualGrossInput.value === '') {
-      annualGrossInput.value = '0';
-      annualNetInput.value = '0';
-      annualNetSalarySpan.textContent = '0';
-      annualGrossSalarySpan.textContent = '0';
-      return;
+    if (input.value === '') {
+      app.annualGrossSalary = 0;
+      app.annualNetSalary = 0;
+      app.monthlyGrossSalary = 0;
+      app.monthlyNetSalary = 0;
+    } else {
+      if (id.includes('annual_gross')) {
+        app.annualGrossSalary = input.value;
+        app.annualNetSalary = app.calculateAnnualNetSalary(app.annualGrossSalary);
+        app.monthlyGrossSalary = app.annualSalaryToMonthly(app.annualGrossSalary);
+        app.monthlyNetSalary = app.annualSalaryToMonthly(app.annualNetSalary);
+      } else if (id.includes('annual_net')) {
+        app.annualNetSalary = input.value;
+        app.annualGrossSalary = app.calculateAnnualGrossSalary(app.annualNetSalary);
+        app.monthlyGrossSalary = app.annualSalaryToMonthly(app.annualGrossSalary);
+        app.monthlyNetSalary = app.annualSalaryToMonthly(app.annualNetSalary);
+      } else if (id.includes('monthly_gross')) {
+        app.monthlyGrossSalary = input.value;
+        app.annualGrossSalary = app.monthlySalaryToAnnual(app.monthlyGrossSalary);
+        app.annualNetSalary = app.calculateAnnualNetSalary(app.annualGrossSalary);
+        app.monthlyNetSalary = app.annualSalaryToMonthly(app.annualNetSalary);
+      } else if (id.includes('monthly_net')) {
+        app.monthlyNetSalary = input.value;
+        app.annualNetSalary = app.monthlySalaryToAnnual(app.monthlyNetSalary);
+        app.annualGrossSalary = app.calculateAnnualGrossSalary(app.annualNetSalary);
+        app.monthlyGrossSalary = app.annualSalaryToMonthly(app.annualGrossSalary);
+      }
     }
 
-    const annualNetSalary = app.calculateAnnualNetSalary(annualGrossInput.value);
-
-    annualNetInput.value = annualNetSalary;
-    annualNetSalarySpan.textContent = annualNetSalary;
-    annualGrossSalarySpan.textContent = annualGrossInput.value;
+    app.updateSalariesDisplay();
   },
 
-  updateAnnualGrossSalary: (evt) => {
-    const annualNetInput = evt.target;
+  updateSalariesDisplay: () => {
+    const annualGrossSalarySpan = document.querySelector('#annual_gross_salary');
+    const annualNetSalarySpan = document.querySelector('#annual_net_salary');
+    const monthlyGrossSalarySpan = document.querySelector('#monthly_gross_salary');
+    const monthlyNetSalarySpan = document.querySelector('#monthly_net_salary');
 
-    const annualGrossInput = document.querySelector('#gross_input');
-    const annualNetSalarySpan = document.querySelector('#net_salary');
-    const annualGrossSalarySpan = document.querySelector('#gross_salary');
+    annualGrossSalarySpan.textContent = app.annualGrossSalary;
+    annualNetSalarySpan.textContent = app.annualNetSalary;
+    monthlyGrossSalarySpan.textContent = app.monthlyGrossSalary;
+    monthlyNetSalarySpan.textContent = app.monthlyNetSalary;
 
-    if (annualNetInput.value === '') {
-      annualNetInput.value = '0';
-      annualGrossInput.value = '0';
-      annualNetSalarySpan.textContent = '0';
-      annualGrossSalarySpan.textContent = '0';
-      return;
-    }
+    const annualGrossInput = document.querySelector('#annual_gross_input');
+    const annualNetInput = document.querySelector('#annual_net_input');
+    const monthlyGrossInput = document.querySelector('#monthly_gross_input');
+    const monthlyNetInput = document.querySelector('#monthly_net_input');
 
-    const annualGrossSalary = app.calculateAnnualGrossSalary(annualNetInput.value);
-
-    annualGrossInput.value = annualGrossSalary;
-    annualGrossSalarySpan.textContent = annualGrossSalary;
-    annualNetSalarySpan.textContent = annualNetInput.value;
+    annualGrossInput.value = app.annualGrossSalary;
+    annualNetInput.value = app.annualNetSalary;
+    monthlyGrossInput.value = app.monthlyGrossSalary;
+    monthlyNetInput.value = app.monthlyNetSalary;
   },
 };
 
